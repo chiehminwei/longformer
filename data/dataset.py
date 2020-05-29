d8cf55a7aee599b3e704ce597327ffb0b7d36c06
Jimmy Wei <icewing1996@gmail.com>
2020-05-29 18:10:35 +0800

    remove printing for debugging

--- data/dataset.py
+++ data/dataset.py
@@ -19,9 +19,6 @@ def pad_to_window_size(input_ids: torch.Tensor, attention_mask: torch.Tensor,
     Returns
         (input_ids, attention_mask) padded to length divisible by 2 * one_sided_window_size
     '''
-    print(input_ids.shape)
-
-
     w = 2 * one_sided_window_size
     seqlen = input_ids.size(1)
     padding_len = (w - seqlen % w) % w
@@ -37,7 +34,6 @@ class MafiascumDataset(Dataset):
 
     tokenizer = LongformerTokenizer.from_pretrained('longformer-base-4096')
     config = LongformerConfig()
-    print(config)
 
     df = pd.read_pickle(data_path, compression="gzip")
     grouped_df = df.groupby(["author", "game_id"])
