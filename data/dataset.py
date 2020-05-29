bd8c24b66049fc8b4d4c731ef19a628577d38f5a
Jimmy Wei <icewing1996@gmail.com>
2020-05-29 21:12:56 +0800

    really

--- data/dataset.py
+++ data/dataset.py
@@ -8,23 +8,7 @@ from transformers import LongformerModel, LongformerTokenizer, LongformerConfig
 MAX_SENTENCE_LEN = 150 # hand picked for our dataset
 MAX_DOC_LEN = 4096 # limited by length of longformer
 
-def pad_to_window_size(input_ids: torch.Tensor, attention_mask: torch.Tensor,
-                       one_sided_window_size: int, pad_token_id: int):
-    '''A helper function to pad tokens and mask to work with the sliding_chunks implementation of Longformer selfattention.
-    Input:
-        input_ids = torch.Tensor(bsz x seqlen): ids of wordpieces
-        attention_mask = torch.Tensor(bsz x seqlen): attention mask
-        one_sided_window_size = int: window size on one side of each token
-        pad_token_id = int: tokenizer.pad_token_id
-    Returns
-        (input_ids, attention_mask) padded to length divisible by 2 * one_sided_window_size
-    '''
-    w = 2 * one_sided_window_size
-    seqlen = input_ids.size(1)
-    padding_len = (w - seqlen % w) % w
-    input_ids = F.pad(input_ids, (0, padding_len), value=pad_token_id)
-    attention_mask = F.pad(attention_mask, (0, padding_len), value=False)  # no attention on the padding tokens
-    return input_ids, attention_mask
+
 
 
 class MafiascumDataset(Dataset):
@@ -74,14 +58,11 @@ class MafiascumDataset(Dataset):
       if num_sentences_in_game < 10:
         continue
 
-      # padding seqlen to the nearest multiple of 512. Needed for the 'sliding_chunks' attention
-      all_sentences_in_game = torch.LongTensor(all_sentences_in_game[:MAX_DOC_LEN]).unsqueeze(0)
-      all_attention_masks_in_game = torch.LongTensor(all_attention_masks_in_game[:MAX_DOC_LEN]).unsqueeze(0)
-      input_ids, attention_mask = pad_to_window_size(
-        all_sentences_in_game, all_attention_masks_in_game, config.attention_window, tokenizer.pad_token_id)
+      input_ids = torch.LongTensor(all_sentences_in_game[:MAX_DOC_LEN])
+      attention_mask = torch.LongTensor(all_attention_masks_in_game[:MAX_DOC_LEN])
       
-      inputs.append(input_ids.squeeze())
-      attention_masks.append(attention_mask.squeeze())
+      inputs.append(input_ids)
+      attention_masks.append(attention_mask)
       labels.append(label)
 
     self.inputs = inputs
