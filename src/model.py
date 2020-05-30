0f0babc6d3460644db43a77b2d1ec993d8b60024
Jimmy Wei <icewing1996@gmail.com>
2020-05-31 00:29:11 +0800

    switch to allenai implementation

--- src/model.py
+++ src/model.py
@@ -11,7 +11,7 @@ class LongformerForSequenceClassification(nn.Module):
     def __init__(self, config):
         super().__init__(config)
         self.num_labels = config.num_labels
-        self.longformer = Longformer.from_pretrained('longformer-base-4096/', config=config, cache_dir = config.cache_dir)
+        self.longformer = Longformer.from_pretrained('../longformer-base-4096/', config=config)
         self.classifier = LongformerClassificationHead(config)
 
     def forward(
